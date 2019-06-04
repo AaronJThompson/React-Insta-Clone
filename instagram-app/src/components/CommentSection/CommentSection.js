cb1bdce03b25f257b949f945e81e14af12be1209
AaronJThompson <aaronjthompson98@gmail.com>
2019-06-04 12:06:46 +0100

    Basic comment container structure

--- instagram-app/src/components/CommentSection/CommentSection.js
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './CommentSection.css';
+import Comment from './Comment';
+
+export default function CommentSection(props) {
+    let { comments } = props;
+
+    return(
+        <div>
+            {
+                comments.map(comment => {
+                    return (
+                        <Comment username={comment.username} text={comment.text} />
+                    )
+                })
+            }
+        </div>
+    )
+}
