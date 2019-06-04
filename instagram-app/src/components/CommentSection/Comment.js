42b704b7f3ccb42234ed2d57cdeb733d7c8b86ed
AaronJThompson <aaronjthompson98@gmail.com>
2019-06-04 12:04:38 +0100

    Basic comment structure

---
+++ instagram-app/src/components/CommentSection/Comment.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './CommentSection.css';
+
+export default function Comment(props) {
+    let { username, text } = props;
+
+    return (
+        <p>
+            <span style={{fontWeight: "bold"}}>
+                {username}
+            </span>
+            {text}
+        </p>
+    )
+}
