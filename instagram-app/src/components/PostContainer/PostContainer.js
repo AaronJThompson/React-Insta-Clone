5ea35e205eb5ba6c89e8ede6bc7afab810e45e98
AaronJThompson <aaronjthompson98@gmail.com>
2019-06-04 11:59:58 +0100

    Basic post container strcuture

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -1,2 +1,22 @@
 import React from 'react';
 import './PostContainer.css';
+import CommentSection from '../CommentSection/CommentSection'
+import { Card, CardBody, CardImg, CardTitle, Container } from 'reactstrap';
+export default function PostContainer(props) {
+    let { username, thumbnailUrl, imageUrl, likes, timestamp, comments } = props;
+
+    return (
+        <div>
+            <Card>
+                <CardTitle>
+                    <img src={thumbnailUrl} alt={username} />
+                    {username}
+                </CardTitle>
+                <CardImg top width="100%" src={imageUrl} />
+                <CardBody>
+                    <CommentSection comments={comments} />
+                </CardBody>
+            </Card>
+        </div>
+    )
+}
